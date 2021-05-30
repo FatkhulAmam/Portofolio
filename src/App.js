51ff318addd22edd277e2f7d2aa5660470db0651
afatkhulamam <afatkhulamam@gmail.com>
2021-05-30 12:50:10 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
+// import view
+import Home from './Views/Home';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/Portofolio" component={Home} exact />
+      </Switch>
+    </BrowserRouter>
   );
 }
-
-export default App;
